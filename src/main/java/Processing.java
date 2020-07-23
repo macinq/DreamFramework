c02127cd2b14c99e0ec7a6001007cb1a1b381dd2
OnlyM1ss <45197509+OnlyM1ss@users.noreply.github.com>
2020-07-23 18:22:38 +0400

    added logging

---
+++ src/main/java/Processing.java
@@ -0,0 +1,7 @@
+public class Processing {
+    public static void typeOfCode(int Code)  {
+        if(Code == 200) {
+
+        }
+    }
+}
--- src/test/java/ChangeRecordTest.java
+++ src/test/java/ChangeRecordTest.java
@@ -1,10 +1,16 @@
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import io.cucumber.java.ru.И;
 import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
+import org.apache.log4j.LogManager;
 import org.junit.BeforeClass;
 import org.junit.Test;
-
+import org.junit.runner.RunWith;
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
 import java.io.IOException;
 
 import static io.restassured.RestAssured.given;
@@ -13,17 +19,24 @@ import static io.restassured.RestAssured.given;
  * меняет запись
  */
 
-
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/features",
+        glue = "ru.dream.framework",
+        tags = "@changeRecord"
+)
 public class ChangeRecordTest {
     /*
     номер поста
      */
+    Logger log = LogManager.getLogger(ChangeRecordTest.class);
     int post = 3;
+
     @BeforeClass
     public static void prepare() throws IOException {
         System.getProperties().load(ClassLoader.getSystemResourceAsStream("my.properties"));
         RestAssured.requestSpecification = new RequestSpecBuilder()
-                .setBaseUri(System.getProperty("Site"))
+                .setBaseUri(System.getProperty("Url"))
                 .addCookie(System.getProperty("Cookie"))
                 .addHeader("X-CSRFToken", System.getProperty("TOKEN"))
                 .setAccept(ContentType.JSON)
@@ -31,7 +44,6 @@ public class ChangeRecordTest {
                 .log(LogDetail.ALL)
                 .build();
     }
-
     @Test
     public void tryChange() {
         ChangeTitle changeTitle = new ChangeTitle();
