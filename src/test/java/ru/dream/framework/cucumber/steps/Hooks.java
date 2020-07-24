e0141336a99afc06feff4fb685339e26c260182c
Dmitrii Prikhozhaev <prigho-dima@ya.ru>
2020-07-24 14:25:44 +0400

    [Логирование:log4j]: добавленно логирование для классов Hooks  и Steps

--- src/test/java/ru/dream/framework/cucumber/steps/Hooks.java
+++ src/test/java/ru/dream/framework/cucumber/steps/Hooks.java
@@ -1,7 +1,9 @@
 package ru.dream.framework.cucumber.steps;
 
 import io.cucumber.java.BeforeStep;
+import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
+import org.testng.annotations.AfterGroups;
 
 import static ru.dream.framework.cucumber.steps.EditProfileSteps.*;
 
@@ -14,14 +16,17 @@ public class Hooks {
     Thread.sleep(3000);
   }
 
-  //@After
+  @AfterGroups
   public void clearDrivers() {
+    logger.info("Завершение сеанса WebDriver");
     if (webDriver.equalsIgnoreCase("google-chrome")) {
       driverChrome.quit();
+      logger.info("Сеанс драйвера Chrome завершен");
     } else if (webDriver.equalsIgnoreCase("mozilla-firefox")) {
       driverFirefox.quit();
+      logger.info("Сеанс драйвера Firefox завершен");
     } else {
-      throw new IllegalArgumentException("Unsupported webdriver: " + webDriver);
+      throw new IllegalArgumentException("Unsupported web driver: " + webDriver);
     }
   }
 }
--- src/test/java/ru/dream/framework/cucumber/steps/Steps.java
+++ src/test/java/ru/dream/framework/cucumber/steps/Steps.java
@@ -20,48 +20,58 @@ public class Steps {
 
     @Given("^указать путь драйвера для браузера Chrome '(.*)'$")
     public void initChrome(String pathDriver){
-
+        logger.info("Инициализация драйвера Chrome");
         System.setProperty("webdriver.chrome.driver", pathDriver);
         driverChrome = new ChromeDriver();
         System.out.println("Двайвер Chrome установлен");
+        logger.info("Инициализация драйвера Chrome прошла успешно");
     }
 
     @Given("^указать путь драйвера для браузера FireFox '(.*)'$")
     public void initFirefox(String pathDriver){
-
+        logger.info("Инициализация драйвера");
         System.setProperty("webdriver.gecko.driver",pathDriver);
         driverFirefox= new FirefoxDriver();
         System.out.println("Двайвер FireFox установлен");
+        logger.info("Инициализация драйвера FireFox прошла успешно");
     }
 
     @When("^открыть страницу '(.*)'$")
     public void openPage(String url){
+        logger.info(String.format("Открытие страницы по url: %s", url));
         driverChrome.manage().window().setSize(new Dimension(900,1000));
         driverFirefox.manage().window().setPosition(new Point(901,10));
         driverFirefox.manage().window().setSize(new Dimension(900,1000));
         driverChrome.get(url);
         driverFirefox.get(url);
         System.out.println("Страница: "+url+" успешно открыта");
+        logger.info("Страница: "+url+" успешно открыта.");
     }
 
     @Then("^найти навигационный бар '(.*)'$")
     public void checkNavBar(String xpathNavBar){
+        logger.info("Поиск навигационного бара");
         driverChrome.findElement(By.xpath(xpathNavBar));
         driverFirefox.findElement(By.xpath(xpathNavBar));
         System.out.println("NavBar присутствует");
+        logger.info("Навигационный бар найден");
     }
 
     @Then("^найти кнопку регистрации '(.*)'$")
     public void checkRegistrationButton(String xpathRegButton){
+        logger.info("Поиск кнопки регистрации");
         driverChrome.findElement(By.xpath(xpathRegButton));
         driverFirefox.findElement(By.xpath(xpathRegButton));
         System.out.println("Кнопка регистрации присутствует");
+        logger.info("Кнопка регистрации найдена");
     }
 
     @Then("^найти кнопку входа '(.*)'$")
     public void checkLoginButton(String xpathLoginButton){
+        logger.info("Поиск кнопки входа");
         driverChrome.findElement(By.xpath(xpathLoginButton));
         driverFirefox.findElement(By.xpath(xpathLoginButton));
         System.out.println("Кнопка входа присутствует");
+        logger.info("Кнопка входа найдена");
     }
 }
