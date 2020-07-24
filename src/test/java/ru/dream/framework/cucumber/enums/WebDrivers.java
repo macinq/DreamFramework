53d6188d22a9e5023295914eb6982148775a9388
Dmitriy <ivanovdm812@gmail.com>
2020-07-24 09:57:33 +0400

    [cucumber]_refact: добавлена возможность в WebDrivers.java получить по текстовому значению из cucumber соответствующий enum

--- src/test/java/ru/dream/framework/cucumber/enums/WebDrivers.java
+++ src/test/java/ru/dream/framework/cucumber/enums/WebDrivers.java
@@ -1,22 +1,93 @@
 package ru.dream.framework.cucumber.enums;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
 public enum WebDrivers {
-  CHROME("webdriver.chrome.driver", "C:\\webdriver\\chromedriver.exe"),
-  FIREFOX("webdriver.gecko.driver", "C:\\webdriver\\geckodriver.exe");
+    CHROME("webdriver.chrome.driver", "C:\\webdriver\\chromedriver.exe") {
+        @Override
+        public String getName() {
+            return "google-chrome";
+        }
 
-  public String type;
-  public String path;
+        @Override
+        protected void initAliases() {
+            aliases.put(this.getName(), new HashSet<>());
+            aliases.get(this.getName()).add(this.getName());
+            aliases.get(this.getName()).add(this.toString());
+            aliases.get(this.getName()).add("'google-chrome'");
+        }
+    },
 
-  WebDrivers(String type, String path) {
-    this.type = type;
-    this.path = path;
-  }
+    FIREFOX("webdriver.gecko.driver", "C:\\webdriver\\geckodriver.exe") {
+        @Override
+        public String getName() {
+            return "mozilla-firefox";
+        }
 
-  public String getType() {
-    return type;
-  }
+        @Override
+        protected void initAliases() {
+            aliases.put(this.getName(), new HashSet<>());
+            aliases.get(this.getName()).add(this.getName());
+            aliases.get(this.getName()).add(this.toString());
+            aliases.get(this.getName()).add("'mozilla-firefox'");
+        }
+    };
 
-  public String getPath() {
-    return path;
-  }
+    private static Map<String, HashSet<String>> aliases;
+
+    static {
+        aliases = new HashMap<>();
+        for (WebDrivers webDrivers : WebDrivers.values()) {
+            webDrivers.initAliases();
+        }
+    }
+
+    public String type;
+    public String path;
+
+    WebDrivers(String type, String path) {
+        this.type = type;
+        this.path = path;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    /**
+     * По имени web-driver возвращает enum значение. Работет
+     * со сценариями из cucumber
+     *
+     * @param alias имя web-driver или его псевдоним
+     * @return WebDrivers, иначе при отсутствии значения возвращает null
+     */
+    public static WebDrivers getValue(String alias) {
+        for (WebDrivers webDriver : WebDrivers.values()) {
+            if (webDriver.hasAlias(alias)) {
+                return webDriver;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Метод показывает если данные псевдоним у соответствующего
+     * экземпляра enum. Если на вход подано имя enum, также возвращает
+     * True
+     * @param alias псевдоним
+     * @return True если такое псевдоним есть или это имя enum. Иначе False
+     */
+    public Boolean hasAlias(String alias) {
+        return aliases.get(this.getName()).contains(alias);
+    }
+
+    public abstract String getName();
+
+    protected abstract void initAliases();
 }
